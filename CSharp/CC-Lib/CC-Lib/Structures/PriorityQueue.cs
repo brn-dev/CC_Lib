8eda29f1618465dda1e6351fb7b76da89e29237f
Dominik Baron <domo.baron@aon.at>
2018-05-03 07:43:01 +0200

    fix indent

--- CSharp/CC-Lib/CC-Lib/Structures/PriorityQueue.cs
+++ CSharp/CC-Lib/CC-Lib/Structures/PriorityQueue.cs
@@ -49,7 +49,7 @@ namespace CC_Lib.Structures
                 var node = _list.First;
                 for (int i = 0; i < index; i++)
                 {
-                    node = node.Next;
+                    node = node?.Next;
                 }
                 return node;
             }
@@ -58,108 +58,108 @@ namespace CC_Lib.Structures
                 var node = _list.Last;
                 for (int i = _list.Count - 1; i > index; i--)
                 {
-                    node = node.Previous;
+                    node = node?.Previous;
                 }
                 return node;
             }
         }
 
         /// <summary>
-            /// Adds an element to the priority queue. If there are already elements in the queue which have the same priority as the given priority, 
-            /// the element will be inserted as last element with the same priority
-            /// </summary>
-            /// <param name="value">The value to be added</param>
-            /// <param name="priority">The priority determining the position of the element</param>
-            /// <returns></returns>
-            public int Add(TV value, TP priority)
-            {
-                int insertIndex = 0;
-
-                var node = _list.First;
-
-                while (node != null)
-                {
-                    if (node.Value.Key.CompareTo(priority) > 0)
-                    {
-                        _list.AddBefore(node, new KeyValuePair<TP, TV>(priority, value));
-                        return insertIndex;
-                    }
-                    node = node.Next;
-                    insertIndex++;
-                }
-
-                _list.AddLast(new KeyValuePair<TP, TV>(priority, value));
-                return insertIndex + 1;
-            }
-
-            /// <summary>
-            /// Returns the value of the element with the lowest priority without removing it.
-            /// </summary>
-            public TV PeekLowest()
-            {
-                var first = _list.First;
-                return first != null ? first.Value.Value : default(TV);
-            }
-
-            /// <summary>
-            /// Returns the value of the element with the lowest priority and removes it.
-            /// </summary>
-            public TV PollLowest()
-            {
-                var first = _list.First;
-                if (first == null)
-                {
-                    return default(TV);
-                }
-                _list.RemoveFirst();
-                return first.Value.Value;
-            }
-
-            /// <summary>
-            /// Returns the value of the element with the highest priority without removing it.
-            /// </summary>
-            public TV PeekHighest()
-            {
-                var last = _list.Last;
-                return last != null ? last.Value.Value : default(TV);
-            }
-
-            /// <summary>
-            /// Returns the value of the element with the highest priority and removes it.
-            /// </summary>
-            public TV PollHighest()
-            {
-                var last = _list.Last;
-                if (last == null)
-                {
-                    return default(TV);
-                }
-                _list.RemoveLast();
-                return last.Value.Value;
-            }
-
-            /// <summary>
-            /// Removes the element at the given index.
-            /// </summary>
-            public void RemoveAt(int index)
-            {
-                int count = Count;
-                if (index < -count || index >= count)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-                _list.Remove(GetNodeAt(index));
-
-            }
-
-            public IEnumerator<TV> GetEnumerator()
-            {
-                return _list.Select(x => x.Value).GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
+        /// Adds an element to the priority queue. If there are already elements in the queue which have the same priority as the given priority, 
+        /// the element will be inserted as last element with the same priority
+        /// </summary>
+        /// <param name="value">The value to be added</param>
+        /// <param name="priority">The priority determining the position of the element</param>
+        /// <returns></returns>
+        public int Add(TV value, TP priority)
         {
-                return GetEnumerator();
+            int insertIndex = 0;
+
+            var node = _list.First;
+
+            while (node != null)
+            {
+                if (node.Value.Key.CompareTo(priority) > 0)
+                {
+                    _list.AddBefore(node, new KeyValuePair<TP, TV>(priority, value));
+                    return insertIndex;
+                }
+                node = node.Next;
+                insertIndex++;
             }
+
+            _list.AddLast(new KeyValuePair<TP, TV>(priority, value));
+            return insertIndex + 1;
+        }
+
+        /// <summary>
+        /// Returns the value of the element with the lowest priority without removing it.
+        /// </summary>
+        public TV PeekLowest()
+        {
+            var first = _list.First;
+            return first != null ? first.Value.Value : default(TV);
+        }
+
+        /// <summary>
+        /// Returns the value of the element with the lowest priority and removes it.
+        /// </summary>
+        public TV PollLowest()
+        {
+            var first = _list.First;
+            if (first == null)
+            {
+                return default(TV);
+            }
+            _list.RemoveFirst();
+            return first.Value.Value;
+        }
+
+        /// <summary>
+        /// Returns the value of the element with the highest priority without removing it.
+        /// </summary>
+        public TV PeekHighest()
+        {
+            var last = _list.Last;
+            return last != null ? last.Value.Value : default(TV);
+        }
+
+        /// <summary>
+        /// Returns the value of the element with the highest priority and removes it.
+        /// </summary>
+        public TV PollHighest()
+        {
+            var last = _list.Last;
+            if (last == null)
+            {
+                return default(TV);
+            }
+            _list.RemoveLast();
+            return last.Value.Value;
+        }
+
+        /// <summary>
+        /// Removes the element at the given index.
+        /// </summary>
+        public void RemoveAt(int index)
+        {
+            int count = Count;
+            if (index < -count || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            _list.Remove(GetNodeAt(index));
+
+        }
+
+        public IEnumerator<TV> GetEnumerator()
+        {
+            return _list.Select(x => x.Value).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
+}
