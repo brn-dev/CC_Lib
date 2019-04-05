a00e2ec0457917e4b2404386f988fa5bb6bf9c72
Dominik Baron <dominik.b4ron@gmail.com>
2019-04-05 10:16:32 +0200

    add Temp algorithm

---
+++ CSharp/CC-Lib/CC-Lib/Algorithms/Temp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CC_Lib.Algorithms
+{
+    class Temp
+    {
+        public IEnumerable<double> Values(double value, IEnumerable<double> manipulators, double differenceMultiplier)
+        {
+            
+            foreach (var next in manipulators)
+            {
+                value += (next - value) * differenceMultiplier;
+                yield return value;
+            }
+        }
+    }
+}
